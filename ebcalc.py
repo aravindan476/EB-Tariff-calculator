82be5b6d39c3b690d00c2e3e0e5cb8cb9bedb21c
Aravindan <53733602+aravindan476@users.noreply.github.com>
2021-08-01 09:15:32 -0700

    Just started
    Just started the python program

---
+++ ebcalc.py
@@ -0,0 +1,6 @@
+# EB Calculator
+
+initial_reading = int(input("Enter the initial reading:"))
+final_reading = int(input("Enter the final reading:"))
+
+consumed_amount = final_reading - initial_reading
